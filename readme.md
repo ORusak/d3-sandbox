f9be1760ecfa814cd8e8e4b7f2d01d5b5d79f849
Oleg Rusak <hare.brown@gmail.com>
2016-05-03 22:53:01 +0300

    init commit

---
+++ readme.md
@@ -0,0 +1,25 @@
+# Description
+Status: development.
+
+d3 diagram sandbox.
+
+* d3js
+* gulp
+
+Demo: 
+
+```
+```
+
+Note: environment tested only os windows; browsers firefox, google chrome.
+
+# Requirement
+
+*  
+
+# Application
+
+* Features
+    *     
+* Roadmap
+    
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const webpack = require('webpack')
+const path = require('path')
+const LiveReloadPlugin = require('webpack-livereload-plugin')
+
+const NODE_ENV = process.env.NODE_ENV || 'development'
+
+module.exports = {
+  entry: {
+    histogram: './app/histogram.js',
+    ['round-chart']: './app/round-chart.js',
+    ['histogram-horizontal']: './app/histogram-horizontal.js',
+    ['histogram-stack']: './app/histogram-stack.js',
+    ['histogram-stack-horizontal']: './app/histogram-stack-horizontal.js'
+  },
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: '[name].js'
+  },
+  devtool: NODE_ENV === 'development' ? 'cheap-inline-module-source-map' : 'source-map',
+  watch: NODE_ENV === 'development',
+  module: {
+    loaders: []
+  },
+  resolve: {
+    modulesDirectories: ['node_modules'],
+    extensions: ['', '.js']
+  },
+  resolveLoader: {
+    modulesDirectories: ['node_modules'],
+    moduleTemplates: ['*-loader', '*'],
+    extensions: ['', '.js']
+  },
+  plugins: [
+    new LiveReloadPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  externals: {
+  }
+}
